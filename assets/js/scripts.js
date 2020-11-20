e16e35a68b0aba34f4bbcde6a9d782016b1675f1
Reinan Santos <reinandesouza01@gmail.com>
2020-11-20 09:41:17 -0300

    Update: resposivity

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,8 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
   var x = document.getElementById("myTopnav");
-  if (x.className === "navbar") {
+  if (x.className === "navItems") {
     x.className += " responsive";
   } else {
-    x.className = "navbar";
+    x.className = "navItems";
   }
-} 
+}
